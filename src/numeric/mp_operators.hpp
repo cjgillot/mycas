8b1679868d2506b26a76f9707d14a83f7a60fe5a
Camille GILLOT <k1000.jlo@wanadoo.fr>
2012-05-09 17:25:19 +0200

    Moved some declarations into flint.hpp

--- src/numeric/mp_operators.hpp
+++ src/numeric/mp_operators.hpp
@@ -8,42 +8,6 @@
 
 #include "flint.hpp"
 
-extern "C" {
-
-/* double */
-void fmpz_set_d(fmpz_t, double);
-void fmpq_set_d(fmpq_t, double);
-double fmpz_get_d(const fmpz_t);
-double fmpq_get_d(const fmpq_t);
-
-/* cmp */
-int fmpz_cmp_d(const fmpz_t, double);
-
-int fmpz_cmpabs_ui(const fmpz_t, ulong);
-
-int fmpq_cmp(const fmpq_t, const fmpq_t);
-int fmpq_cmp_ui(const fmpq_t, ulong, ulong);
-int fmpq_cmp_si(const fmpq_t,  long, ulong);
-
-/* set */
-#define STR(x) (const_cast<char*>(x))
-// int fmpz_set_str(fmpz_t, const char*, int);
-int  fmpq_set_str(fmpq_t, const char*, int);
-void fmpz_set_q(fmpz_t, const fmpq_t);
-void mpfr_set_fz(mpfr_ptr, const fmpz_t, mpfr_rnd_t);
-
-/* bitwise */
-void fmpz_com(fmpz_t, const fmpz_t);
-void fmpz_and(fmpz_t, const fmpz_t, const fmpz_t);
-void fmpz_ior(fmpz_t, const fmpz_t, const fmpz_t);
-void fmpz_xor(fmpz_t, const fmpz_t, const fmpz_t);
-
-/* hash */
-size_t fmpz_hash(const fmpz_t);
-size_t fmpq_hash(const fmpq_t);
-size_t mpfr_hash(mpfr_srcptr);
-}
-
 // routines in gmpfrxx.cpp
 std::istream& fmpfr_parse(std::istream &, fmpz_t);
 std::istream& fmpfr_parse(std::istream &, fmpq_t);
